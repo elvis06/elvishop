883303497b92d19c72429390729d1fce2e3b99f7
elvis_ebm <elvis.ebm06@gmail.com>
2020-10-01 23:11:53 -0400

    Prueba login1

--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -892,11 +892,11 @@
 
                                         <!-- Login Buttons -->
                                         <div class="d-flex">
-                                            <a class="btn btn-block btn-sm btn-soft-facebook transition-3d-hover mr-1" href="{{url('login/facebook'}}">
+                                            <a class="btn btn-block btn-sm btn-soft-facebook transition-3d-hover mr-1" href="{{url('login/facebook')}}">
                                               <span class="fab fa-facebook-square mr-1"></span>
                                               Facebook
                                             </a>
-                                            <a class="btn btn-block btn-sm btn-soft-google transition-3d-hover ml-1 mt-0" href="{{url('login/google'}}">
+                                            <a class="btn btn-block btn-sm btn-soft-google transition-3d-hover ml-1 mt-0" href="{{url('login/google')}}">
                                               <span class="fab fa-google mr-1"></span>
                                               Google
                                             </a>
