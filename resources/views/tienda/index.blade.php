c0e1e7475ba3255a5759a0f2bf256376928d6144
unknown <elvis.ebm06@gmail.com>
2021-01-16 14:26:06 -0400

    mod18

--- resources/views/tienda/index.blade.php
+++ resources/views/tienda/index.blade.php
@@ -101,14 +101,14 @@
 		<div class="mb-5">
 			<div class="row">
 				<div class="col-md-6 mb-4 mb-xl-0 col-xl-3">
-					<a href="capsulas-maxman-xl" class="d-black text-gray-90">
+					<a href="reduce-extreme" class="d-black text-gray-90">
 						<div class="min-height-132 py-1 d-flex bg-gray-1 align-items-center">
 							<div class="col-6 col-xl-5 col-wd-6 pr-0">
 								<img class="img-fluid" src="../../assets/img/190X150/img1.png" alt="Image Description">
 							</div>
 							<div class="col-6 col-xl-7 col-wd-6">
 								<div class="mb-2 pb-1 font-size-18 font-weight-light text-ls-n1 text-lh-23">
-									CAPSULAS MAXMAN <strong>XL</strong>
+									REDUCE EXTREME <strong>Premium</strong>
 								</div>
 								<div class="link text-gray-90 font-weight-bold font-size-15" href="#">
 									Comprar ahora
