cb5613d8b84acaa2ee6704d18fb65cfe1fa7e836
unknown <elvis.ebm06@gmail.com>
2021-01-16 10:44:54 -0400

    mod7

--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -729,7 +729,7 @@
                                     <!-- End List Group -->
                                 </div>
 
-                                <div class="col-12 col-md mb-2 mb-md-0">
+                                <div>
                                     <!-- List Group -->
                                     
                                     <!-- End List Group -->
