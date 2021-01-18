caa6d6b6f7fa7b1755530e38aafa9da4921668fa
unknown <elvis.ebm06@gmail.com>
2021-01-18 10:40:26 -0400

    mod27

--- resources/views/tienda/cart.blade.php
+++ resources/views/tienda/cart.blade.php
@@ -1,6 +1,7 @@
 @extends('plantilla.plantilla')
 @section('titulo','SNDSUR | Salud Natural del Sur - Garcinia - Meizi - Paizhisu - Maxman')
 @section('contenido')
+<?php $productos = ''; ?>
 <!-- ========== MAIN CONTENT ========== -->
 <main id="content" role="main">
     <!-- breadcrumb -->
@@ -44,6 +45,7 @@
                     </thead>
                     <tbody>
                         @foreach($cart as $item)
+                        <?php $productos = {{ $item->nombre }}; ?>
                         <tr class="">
                             <td class="text-center">
                                 <a href="{{ route('cart-delete', $item->slug) }}" class="text-gray-32 font-size-26">×</a>
@@ -124,7 +126,7 @@
                                         <div class="d-block d-md-flex flex-center-between">
                                             <div class="d-md-flex">
                                                 <a href="/" class="btn btn-soft-secondary mb-3 mb-md-0 font-weight-normal px-5 px-md-4 px-lg-5 w-100 w-md-auto">Seguir comprando</a>
-                                                <a href="https://api.whatsapp.com/send?phone=59174505444&text=Hola%20www.saludnaturaldelsur.com%0Quiero%20realizar%20el%20siguiente%20pedido!%0{{number_format($total,2)}}" class="btn btn-primary-dark-w ml-md-2 px-5 px-md-4 px-lg-5 w-100 w-md-auto d-none d-md-inline-block">Realizar pedido</a>
+                                                <a href="https://api.whatsapp.com/send?phone=59174505444&text=Hola%20www.saludnaturaldelsur.com%0Quiero%20realizar%20el%20siguiente%20pedido!%0$productos{{number_format($total,2)}}" class="btn btn-primary-dark-w ml-md-2 px-5 px-md-4 px-lg-5 w-100 w-md-auto d-none d-md-inline-block">Realizar pedido</a>
                                             </div>
                                         </div>
                                     </div>
