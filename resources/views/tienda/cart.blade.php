15b72921a35d1ca6d3d658f81a9551302db877f7
unknown <elvis.ebm06@gmail.com>
2021-01-18 11:05:32 -0400

    mod37

--- resources/views/tienda/cart.blade.php
+++ resources/views/tienda/cart.blade.php
@@ -45,7 +45,7 @@
                     </thead>
                     <tbody>
                         @foreach($cart as $item)
-                        @$productos = $productos . '%0' . {{ $item->nombre }};
+                        {{ $productos = $productos . '%0' . $item->nombre }};
                         <tr class="">
                             <td class="text-center">
                                 <a href="{{ route('cart-delete', $item->slug) }}" class="text-gray-32 font-size-26">×</a>
